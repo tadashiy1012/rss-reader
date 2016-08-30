e014f0108f20470f6798a3e7abb75afab2df4ff4
tadashiy1012 <tada4.ya1986+gh@gmail.com>
2016-08-31 03:16:00 +0900

    create Content

---
+++ app/components/Content.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import Main from './Main.jsx';
+import Settings from './Settings.jsx';
+
+const content = ({content}) => (
+  <div>
+    {content === 'main' ? <Main /> : <Settings />}
+  </div>
+);
+
+const mapStateToProps = (state, props) => {
+  return {
+    content: state.content
+  };
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {};
+};
+
+const Content = connect(mapStateToProps, mapDispatchToProps)(content);
+
+export default Content;
