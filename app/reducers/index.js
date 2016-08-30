5eb39fd7f392c07543edffa48decd9d899ed8a36
tadashiy1012 <tada4.ya1986+gh@gmail.com>
2016-08-31 03:15:16 +0900

    add contents select actions

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,15 @@
 import {handleAction, handleActions} from 'redux-actions';
-import {} from '../actions';
+import {slctMain, slctSet} from '../actions';
 
-const reducer = handleActions({}, {});
+const reducer = handleActions({
+  [slctMain]: (state, action) => Object.assign({}, state, {
+    content: action.payload
+  }),
+  [slctSet]: (state, action) => Object.assign({}, state, {
+    content: action.payload
+  })
+}, {
+  content: 'main'
+});
 
 export default reducer;
