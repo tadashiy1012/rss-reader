00eb5bf9313e792a3d96b37697432bddf3cbee58
tadashiy1012 <tada4.ya1986+gh@gmail.com>
2016-08-31 03:16:16 +0900

    add Header and Content

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,25 @@ import {render} from 'react-dom';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider, connect} from 'react-redux';
 import reducer from './reducers';
+import Header from './components/Header.jsx';
+import Content from './components/Content.jsx';
 
-const App = () => (
+const app = () => (
   <div>
-    <h1>rss-reader</h1>
+    <Header />
+    <Content />
   </div>
 );
+
+const mapStateToProps = (state, props) => {
+  return state;
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {};
+};
+
+const App = connect(mapStateToProps, mapDispatchToProps)(app);
 
 const store = createStore(reducer);
 
