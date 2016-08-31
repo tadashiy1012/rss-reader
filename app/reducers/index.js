c41633d73d18a4d3f62d9e67f51fbc41407e4e70
tadashiy1012 <tada4.ya1986+gh@gmail.com>
2016-09-01 03:11:10 +0900

    add setShows and showCard reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,5 +1,6 @@
 import {handleAction, handleActions} from 'redux-actions';
-import {slctMain, slctSet} from '../actions';
+import {slctMain, slctSet,
+  setShows, showCard} from '../actions';
 
 const reducer = handleActions({
   [slctMain]: (state, action) => Object.assign({}, state, {
@@ -7,9 +8,25 @@ const reducer = handleActions({
   }),
   [slctSet]: (state, action) => Object.assign({}, state, {
     content: action.payload
-  })
+  }),
+  [setShows]: (state, action) => Object.assign({}, state, {
+    cardShows: action.payload
+  }),
+  [showCard]: (state, action) => {
+    console.log(action.payload);
+    let obj = Object.assign({}, state.cardShows, {
+      [action.payload[0]]: action.payload[1]
+    });
+    console.log(obj);
+    let result = Object.assign({}, state, {
+      cardShows: obj
+    });
+    console.log(result);
+    return result;
+  }
 }, {
-  content: 'main'
+  content: 'main',
+  cardShows: {hoge:'hoge'}
 });
 
 export default reducer;
