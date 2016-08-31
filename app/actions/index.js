4dd25353ec853fc8dc274bf4a3ca943ccc618202
tadashiy1012 <tada4.ya1986+gh@gmail.com>
2016-09-01 03:10:33 +0900

    add setShows and showCard actions

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,3 +2,5 @@ import {createAction} from 'redux-actions';
 
 export const slctMain = createAction('SLCT_MAIN');
 export const slctSet = createAction('SLCT_SET');
+export const setShows = createAction('SET_SHOWS');
+export const showCard = createAction('SHOW_CARD');
