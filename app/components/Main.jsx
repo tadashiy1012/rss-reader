91ef65943d388dd0116e643d563006924047c0ef
tadashiy1012 <tada4.ya1986+gh@gmail.com>
2016-09-01 03:12:29 +0900

    modify

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {setShows} from '../actions';
+import ListItem from './ListItem.jsx';
 
-const main = () => (
+let init = false;
+const items = ['hogehoge', 'fugafuga', 'piyopiyo'];
+
+const main = ({handleShows}) => {
+  const obj = {};
+  const nodes = items.map((val, idx) => {
+    obj[idx] = false;
+    return (<ListItem key={idx} idx={idx} text={val} />);
+  });
+  if (!init) {
+    console.log('init');
+    init = true;
+    handleShows(obj);
+  }
+  return (
   <div>
     <h1>main</h1>
+    <ul className="collection">
+      {nodes}
+    </ul>
   </div>
-);
+  );
+}
 
 const mapStateToProps = (state, props) => {
   return state;
 };
 
 const mapDispatchToProps = (dispatch, props) => {
-  return {};
+  return {
+    handleShows: (obj) => {
+      dispatch(setShows(obj));
+    }
+  };
 };
 
 const Main = connect(mapStateToProps, mapDispatchToProps)(main);
