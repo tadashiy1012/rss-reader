6b1614490d9c7d1d9a4e2fc5c575dd61f398af0a
tadashiy1012 <yama3ki.tada4@gmail.com>
2016-09-14 03:24:46 +0900

    add url add form

--- app/components/Settings.jsx
+++ app/components/Settings.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+const sampleData = [
+  'hogehoge',
+  'fugafuga',
+  'piyopiyo'
+];
+
+const Item = ({text}) => (
+  <li className="collection-item">
+    <span>{text}</span>
+    <a className="secondary-content">
+      <i className="material-icons">delete</i>
+    </a>
+  </li>
+);
+
+const sampleItems = () => {
+  return sampleData.map((data) => {
+    return (<Item text={data} />);
+  });
+}
+
 const settings = () => (
   <div>
     <h1>settings</h1>
+    <div className="input-field col s12">
+      <input type="text" className="validate" placeholder="url" />
+    </div>
+    <div>
+      <button className="btn-large waves-effect waves-light center-align">Add URL
+        <i className="material-icons right">get_app</i>
+      </button>
+    </div>
+    <ul className="collection">
+      {sampleItems()}
+    </ul>
   </div>
 );
 
