4b66fe57d5bb3f7d50a398c6ac4ba1dbd9d43f32
tadashiy1012 <yama3ki.tada4@gmail.com>
2016-09-14 04:23:35 +0900

    add url actions

--- app/actions/index.js
+++ app/actions/index.js
@@ -4,3 +4,7 @@ export const slctMain = createAction('SLCT_MAIN');
 export const slctSet = createAction('SLCT_SET');
 export const setShows = createAction('SET_SHOWS');
 export const showCard = createAction('SHOW_CARD');
+export const addUrl = createAction('ADD_URL');
+export const delUrl = createAction('DEL_URL');
+export const flushUrls = createAction('FLUSH_URLS');
+export const changeUrlInputVal = createAction('CHANGE_URL_INPUT_VAL');
