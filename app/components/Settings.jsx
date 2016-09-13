96cfc412b4a3f65b47df8d361d86d72c1bca0d1c
tadashiy1012 <yama3ki.tada4@gmail.com>
2016-09-14 04:24:20 +0900

    modify url action func

--- app/components/Settings.jsx
+++ app/components/Settings.jsx
@@ -1,50 +1,56 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {addUrl, delUrl, changeUrlInputVal} from '../actions';
 
-const sampleData = [
-  'hogehoge',
-  'fugafuga',
-  'piyopiyo'
-];
-
-const Item = ({text}) => (
+const Item = ({text, handleUrlDel}) => (
   <li className="collection-item">
     <span>{text}</span>
-    <a className="secondary-content">
+    <a href="#" className="secondary-content" onClick={() => {handleUrlDel(text)}}>
       <i className="material-icons">delete</i>
     </a>
   </li>
 );
 
-const sampleItems = () => {
-  return sampleData.map((data) => {
-    return (<Item text={data} />);
-  });
-}
+const loadUrls = (urls, func) => {
+  return urls.map((url, idx) => (<Item key={idx} text={url} handleUrlDel={func} />));
+};
 
-const settings = () => (
+const settings = ({value, urls, handleValChange, handleUrlDel, handleUrlAdd}) => (
   <div>
     <h1>settings</h1>
     <div className="input-field col s12">
-      <input type="text" className="validate" placeholder="url" />
+      <input type="text" className="validate" placeholder="url" value={value} onChange={handleValChange} />
     </div>
     <div>
-      <button className="btn-large waves-effect waves-light center-align">Add URL
+      <button className="btn-large waves-effect waves-light center-align" onClick={handleUrlAdd}>Add URL
         <i className="material-icons right">get_app</i>
       </button>
     </div>
     <ul className="collection">
-      {sampleItems()}
+      {loadUrls(urls, handleUrlDel)}
     </ul>
   </div>
 );
 
 const mapStateToProps = (state, props) => {
-  return state;
+  return {
+    value: state.urlInputVal,
+    urls: state.urls
+  };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
-  return {};
+  return {
+    handleUrlAdd: () => {
+      dispatch(addUrl());
+    },
+    handleUrlDel: (url) => {
+      dispatch(delUrl(url));
+    },
+    handleValChange: (ev) => {
+      dispatch(changeUrlInputVal(ev.target.value));
+    }
+  };
 };
 
 const Settings = connect(mapStateToProps, mapDispatchToProps)(settings);
