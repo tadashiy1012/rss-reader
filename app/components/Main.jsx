d0a7b6038414b49635067b2765d3195906e87942
tadashiy1012 <yama3ki.tada4@gmail.com>
2016-09-19 00:28:18 +0900

    modify init proc

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setShows, loadUrls, readFeeds, addItems} from '../actions';
+import {setFeeds, addItems, setShows} from '../actions';
 import ListItem from './ListItem.jsx';
 import {Promise} from 'es6-promise';
 import * as reader from 'rss-reader-lib';
-const {ipcRenderer} = window.require('electron');
 
 let init = false;
 
@@ -22,32 +21,32 @@ const read = (urls) => {
   });
 };
 
-const main = ({urls, feeds, items, handleLoad, handleShows, handleAddItems}) => {
-  const shows = {};
-  const nodes = items.map((val, idx) => {
-    shows[idx] = false;
-    return (<ListItem key={idx} idx={idx} obj={val} />);
-  });
-    if (!init) {
-    init = true;
-    handleLoad();
-    handleShows(shows);
-  }
-  read(urls).then((resp) => {
-    console.log(resp);
-    const ary = [];
-    for (let feeds of resp) {
-      const title = feeds[0];
-      for (let feed of feeds[1]) {
-        feed.title = title + ':' + feed.title;
-        ary.push(feed);
-      }
+const proc = (src) => {
+  const ary = [];
+  let count = 0;
+  for (let feeds of src) {
+    const title = feeds[0];
+    for (let feed of feeds[1]) {
+      feed.idx = (count += 1);
+      feed.title = title + ':' + feed.title;
+      ary.push(feed);
     }
-    const sorted = ary.sort((a, b) => {
-      return new Date(a.date).getTime - new Date(b.date).getTime;
-    }).reverse();
-    console.log(sorted);
-    handleAddItems(sorted);
+  }
+  const sorted = ary.sort((a, b) => {
+    return new Date(a.date).getTime - new Date(b.date).getTime;
+  }).reverse();
+  return sorted;
+};
+
+const main = ({urls, feeds, handleSetFeeds, handleSetShows}) => {
+  if (!init) {
+    init = true;
+    read(urls).then((resp) => {
+      handleSetFeeds(proc(resp));
+    });
+  }
+  const nodes = feeds.map((val, idx) => {
+    return (<ListItem key={idx} obj={val} />);
   });
   return (
   <div>
@@ -60,24 +59,17 @@ const main = ({urls, feeds, items, handleLoad, handleShows, handleAddItems}) => 
 }
 
 const mapStateToProps = (state, props) => {
-  console.log(state);
   return {
     urls: state.urls,
-    feeds: state.feeds,
-    items: state.items
+    feeds: state.feeds
   };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
   return {
-    handleShows: (obj) => {
-      dispatch(setShows(obj));
-    },
-    handleLoad: () => {
-      dispatch(loadUrls(ipcRenderer.sendSync('loadUrls')));
-    },
-    handleAddItems: (arg) => {
-      dispatch(addItems(arg));
+    handleSetFeeds: (arg) => {
+      console.log(arg);
+      dispatch(setFeeds(arg));
     }
   };
 };
